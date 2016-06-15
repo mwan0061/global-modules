f81dfff93cfa3b325dace478ce8a9edfe16bcc65
Max Wan <mwan0061@gmail.com>
2016-06-16 07:33:16 +1000

    global modules

---
+++ devtool/lib/timer-hook.js
@@ -0,0 +1,20 @@
+/*
+  Shims global timers to behave like node,
+  returning values with ref() and unref().
+  This is on by default; you can disable with
+  --no-node-timers or --no-nt
+ */
+
+var timers = require('timers');
+
+module.exports = function timerHook () {
+  global.setTimeout = wrapWithActivateUvLoop(timers.setTimeout);
+  global.setInterval = wrapWithActivateUvLoop(timers.setInterval);
+};
+
+function wrapWithActivateUvLoop (func) {
+  return function updateTimerLoop () {
+    process.activateUvLoop();
+    return func.apply(this, arguments);
+  };
+}
